cc38569f3ba264e6f4c43af4c13364ec3ddb3d7b
csaave25 <csaavedra@emt.cl>
2024-05-23 00:02:28 -0400

    Se agregó el navbar

--- src/app/movies/movies-routing.module.ts
+++ src/app/movies/movies-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Route, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 
 
 const routes: Route[] = [
     {
         path: '',
-        component: HomeComponent
+        component: LayoutPageComponent,
+        children: [
+            {path: '', component: HomeComponent}
+        ]
     }
 ]
 
--- src/app/movies/movies.module.ts
+++ src/app/movies/movies.module.ts
@@ -3,17 +3,21 @@ import { CommonModule } from '@angular/common';
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 import { HomeComponent } from './pages/home/home.component';
 import { MoviesRoutingModule } from './movies-routing.module';
+import { Mdb5Module } from '../mdb5/mdb5.module';
+import { NavbarComponent } from './pages/layout-page/components/navbar/navbar.component';
 
 
 
 @NgModule({
   declarations: [
     LayoutPageComponent,
-    HomeComponent
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     CommonModule,
-    MoviesRoutingModule
+    MoviesRoutingModule,
+    Mdb5Module
   ]
 })
 export class MoviesModule { }
